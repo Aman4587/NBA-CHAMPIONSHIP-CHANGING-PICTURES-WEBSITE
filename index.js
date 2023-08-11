387865ee783a98ffef8081001a2e05eb4e46e356
Aman4587 <3pointaman@gmail.com>
2023-08-11 14:14:24 -0700

    OK

---
+++ index.js
@@ -0,0 +1,10 @@
+var showing = false 
+document.querySelector("img").style.display = "none"
+function show_team (){
+    document.querySelector("img").style.display = "block"
+    showing = true
+}
+function hide_team(){
+    document.querySelector("img").style.display = "none"
+    showing=false
+}
